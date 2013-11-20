cf36af76d63e5a10f8fc66dd58e8557eb1dacf5e
Logan Koester <logan@logankoester.com>
2013-11-20 02:32:21 -0500

    Removes unused nodeunit boilerplate and grunt dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -21,28 +21,11 @@ module.exports = function(grunt) {
       options: {
         jshintrc: '.jshintrc',
       },
-    },
-
-    // Before generating any new files, remove any previously-created files.
-    clean: {
-      tests: ['tmp'],
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
-    },
-
+    }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'nodeunit']);
 
   // By default, lint and run all tests.
   grunt.registerTask('default', ['jshint', 'test']);
